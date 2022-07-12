ca3ef44c4012c794695a373dbb83bea1aa9845fa
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-07-12 08:41:16 -0400

    Update submitForm.js

--- submitForm.js
+++ submitForm.js
@@ -1,6 +1,6 @@
 let submit = document.getElementById('submit')
 console.log(submit)
-const formName = 'trainingTracker'
+const formName = 'consultationFeeSummary'
 console.log('form: ' + formName)
 let newForm = {}
 
@@ -11,75 +11,124 @@ clientId.addEventListener('change', (e) => {
   console.log(newForm.clientId);
   })
   
-let date = document.querySelector('input#date')
-date.addEventListener('change', (e) => {
-	newForm.date = e.target.value;
-  console.log(newForm.date);
+let employeeName = document.querySelector('input#employeeName')
+employeeName.addEventListener('change', (e) => {
+	newForm.employeeName = e.target.value;
+  console.log(newForm.employeeName);
   })
   
-let behaviors = document.getElementById('behaviors')
-behaviors.addEventListener('change', (e) => {
-	newForm.behaviors = e.target.value;
-  console.log(newForm.behaviors);
+let date1 = document.getElementById('date1')
+date1.addEventListener('change', (e) => {
+	newForm.date1 = e.target.value;
+  console.log(newForm.date1);
+})
+  
+let date2 = document.getElementById('date2')
+date2.addEventListener('change', (e) => {
+	newForm.date2 = e.target.value;
+  console.log(newForm.date2);
+})
+  
+let date3 = document.getElementById('date3')
+date3.addEventListener('change', (e) => {
+	newForm.date3 = e.target.value;
+  console.log(newForm.date3);
   })
   
-let sensory = document.getElementById('sensoryActivity')
-sensory.addEventListener('change', (e) => {
-	newForm.sensory = e.target.value;
-  console.log(newForm.sensory);
-  })
+let date1 = document.getElementById('date1')
+date1.addEventListener('change', (e) => {
+	newForm.date1 = e.target.value;
+  console.log(newForm.date1);
+})
   
-let movement = document.getElementById('movement')
-movement.addEventListener('change', (e) => {
-	newForm.movement = e.target.value;
-  console.log(newForm.movement);
-  })
-
-let learning = document.getElementById('learning')
-learning.addEventListener('change', (e) => {
-	newForm.learning = e.target.value;
-  console.log(newForm.learning);
-  })
-
-let dataCollection = document.getElementById('dataCollection')
-dataCollection.addEventListener('change', (e) => {
-	newForm.dataCollection = e.target.value;
-  console.log(newForm.dataCollection);
-  })
-
-let client = document.getElementById('client')
-client.addEventListener('change', (e) => {
-	newForm.client = e.target.value;
-  console.log(newForm.client);
-  })
-
-let thingsNeeds = document.getElementById('thingsNeeds')
-thingsNeeds.addEventListener('change', (e) => {
-	newForm.thingsNeeds = e.target.value;
-  console.log(newForm.thingsNeeds);
-  })
+let date2 = document.getElementById('date2')
+date2.addEventListener('change', (e) => {
+	newForm.date2 = e.target.value;
+  console.log(newForm.date2);
+})
   
-let followUp = document.getElementById('followUp')
-followUp.addEventListener('change', (e) => {
-	newForm.followUp = e.target.value;
-  console.log(newForm.followUp);
+let date3 = document.getElementById('date3')
+date3.addEventListener('change', (e) => {
+	newForm.date3 = e.target.value;
+  console.log(newForm.date3);
   })
 
-let notes = document.getElementById('notes')
-notes.addEventListener('change', (e) => {
-	newForm.notes = e.target.value;
-  console.log(newForm.notes);
-  })
+  let min1 = document.getElementById('min1')
+min1.addEventListener('change', (e) => {
+	newForm.min1 = e.target.value;
+  console.log(newForm.min1);
+})
+  
+let min2 = document.getElementById('min2')
+min2.addEventListener('change', (e) => {
+	newForm.min2 = e.target.value;
+  console.log(newForm.min2);
+})
+  
+let min3 = document.getElementById('min3')
+min3.addEventListener('change', (e) => {
+	newForm.min3 = e.target.value;
+  console.log(newForm.date3);
+})
+  
+let goal1 = document.getElementById('goal1')
+goal1.addEventListener('change', (e) => {
+	newForm.goal1 = e.target.value;
+  console.log(newForm.goal1);
+})
+  
+let goal2 = document.getElementById('goal2')
+goal2.addEventListener('change', (e) => {
+	newForm.goal2 = e.target.value;
+  console.log(newForm.goal2);
+})
+  
+let goal3 = document.getElementById('goal3')
+goal3.addEventListener('change', (e) => {
+	newForm.goal3 = e.target.value;
+  console.log(newForm.goal3);
+})
+  
+let hour1 = document.getElementById('hour1')
+hour1.addEventListener('change', (e) => {
+	newForm.hour1 = e.target.value;
+  console.log(newForm.hour1);
+})
+  
+let hour2 = document.getElementById('hour2')
+hour2.addEventListener('change', (e) => {
+	newForm.hour2 = e.target.value;
+  console.log(newForm.hour2);
+})
+  
+let hour3 = document.getElementById('hour3')
+hour3.addEventListener('change', (e) => {
+	newForm.hour3 = e.target.value;
+  console.log(newForm.hour3);
+})
 
-let smoothMoves = document.getElementById('smoothMoves')
-smoothMoves.addEventListener('change', (e) => {
-	newForm.smoothMoves = e.target.value;
-  console.log(newForm.smoothMoves);
-  })
+let serviceSummary1 = document.getElementById('serviceSummary1')
+serviceSummary1.addEventListener('change', (e) => {
+	newForm.serviceSummary1 = e.target.value;
+  console.log(newForm.serviceSummary1);
+})
+  
+let serviceSummary2 = document.getElementById('serviceSummary2')
+serviceSummary2.addEventListener('change', (e) => {
+	newForm.serviceSummary2 = e.target.value;
+  console.log(newForm.serviceSummary2);
+})
+  
+let serviceSummary3 = document.getElementById('serviceSummary3')
+serviceSummary3.addEventListener('change', (e) => {
+	newForm.serviceSummary3 = e.target.value;
+  console.log(newForm.serviceSummary3);
+})
+
 
 document.getElementById('submit').addEventListener("click", async (event) => {
 		console.log('click')
-    submitForm(newForm, 'trainingTracker')
+    submitForm(newForm, form)
 })
 
 async function submitForm(data, form) {
@@ -114,4 +163,5 @@ function showSuccess() {
 function showError(err) {
     console.error
     document.getElementById('returnMessage').innerHTML = `An error occurred when submitting this form, which was ${err}. Please contact the administrator for help.`
+}
 }
