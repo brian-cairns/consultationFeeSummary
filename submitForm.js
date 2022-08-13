04288ce697f437d723cc193ccde8e87a3c594ea6
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-08-13 09:53:51 -0400

    full update

--- submitForm.js
+++ submitForm.js
@@ -1,121 +1,102 @@
 let submit = document.getElementById('submit')
+let commit = document.getElementById('commit')
+let addMore = document.getElementById('addMore')
 console.log(submit)
 const formName = 'consultationFee'
 console.log('form: ' + formName)
 let newForm = {}
 let client = ''
+let start = 0
+let timeEntries = []
+let dates = []
+let goals = []
+let mins = []
+let hrs =[]
+let serviceSummaries = []
+submit.style.display = 'none'
+addMore.style.display = 'none'
+submission = {}
+
+class TimeEntry {
+    constructor(date, goal, time, hours, minutes, summary) {
+        this.date = date;
+        this.goal = goal;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.summary = summary;
+        this.time = this.hours + ':' + this.minutes
+    }
+
+}
+
+commit.addEventListener('click', (e) => {
+    let timeEntries = getTimeEntries()
+    console.log(timeEntries)
+    submission.timeEntries = timeEntries
+    submit.style.display = 'inline'
+    addMore.style.display = 'inline'
+    
+})
+
+function getTimeEntries() {
+     dates[1] = document.getElementById('date1').value
+     dates[2] = document.getElementById('date2').value
+     dates[3] = document.getElementById('date3').value
+     mins[1] = document.getElementById('min1').value
+     mins[2] = document.getElementById('min2').value
+     mins[3] = document.getElementById('min3').value
+     goals[1] = document.getElementById('goal1').value
+     goals[2] = document.getElementById('goal2').value
+     goals[3] = document.getElementById('goal3').value
+     hrs[1] = document.getElementById('hour1').value
+     hrs[2] = document.getElementById('hour2').value
+     hrs[3] = document.getElementById('hour3').value
+     serviceSummaries[1] = document.getElementById('serviceSummary1').value
+     serviceSummaries[2] = document.getElementById('serviceSummary2').value
+     serviceSummaries[3] = document.getElementById('serviceSummary3').value
+    for (let i = start; i < start + 3; i++) {
+        let entry = new TimeEntry
+        if (dates[i+1]) {
+            entry = {
+                date: dates[i+1],
+                goal: goals[i + 1],
+                hours: hrs[i + 1],
+                minutes: mins[i + i],
+                summary: serviceSummaries[i + 1]
+            }
+        } else {
+            entry = {
+                date: '',
+                goal: '',
+                hours: '',
+                minutes: '',
+                summary: ''
+            }
+        }
+        console.log(entry)
+        timeEntries.push(entry);
+                    
+    }
+    start = start + 3
+    return timeEntries
+}
 
 let clientId = document.querySelector('input#clientName')
 clientId.addEventListener('change', (e) => {
-	console.log('changed')
-	newForm.clientId = e.target.value;
-  console.log(newForm.clientId);
-  client = newForm.clientId
-  })
-  
+submission.clientName = e.target.value})
+
 let employeeName = document.querySelector('input#employeeName')
 employeeName.addEventListener('change', (e) => {
-	newForm.employeeName = e.target.value;
-  console.log(newForm.employeeName);
-  })
-  
-let date1 = document.getElementById('date1')
-date1.addEventListener('change', (e) => {
-	newForm.date1 = e.target.value;
-  console.log(newForm.date1);
-})
-  
-let date2 = document.getElementById('date2')
-date2.addEventListener('change', (e) => {
-	newForm.date2 = e.target.value;
-  console.log(newForm.date2);
-})
-  
-let date3 = document.getElementById('date3')
-date3.addEventListener('change', (e) => {
-	newForm.date3 = e.target.value;
-  console.log(newForm.date3);
-  })
-  
-  let min1 = document.getElementById('min1')
-min1.addEventListener('change', (e) => {
-	newForm.min1 = e.target.value;
-  console.log(newForm.min1);
-})
-  
-let min2 = document.getElementById('min2')
-min2.addEventListener('change', (e) => {
-	newForm.min2 = e.target.value;
-  console.log(newForm.min2);
-})
-  
-let min3 = document.getElementById('min3')
-min3.addEventListener('change', (e) => {
-	newForm.min3 = e.target.value;
-  console.log(newForm.date3);
-})
-  
-let goal1 = document.getElementById('goal1')
-goal1.addEventListener('change', (e) => {
-	newForm.goal1 = e.target.value;
-  console.log(newForm.goal1);
-})
-  
-let goal2 = document.getElementById('goal2')
-goal2.addEventListener('change', (e) => {
-	newForm.goal2 = e.target.value;
-  console.log(newForm.goal2);
-})
-  
-let goal3 = document.getElementById('goal3')
-goal3.addEventListener('change', (e) => {
-	newForm.goal3 = e.target.value;
-  console.log(newForm.goal3);
-})
-  
-let hour1 = document.getElementById('hour1')
-hour1.addEventListener('change', (e) => {
-	newForm.hour1 = e.target.value;
-  console.log(newForm.hour1);
-})
-  
-let hour2 = document.getElementById('hour2')
-hour2.addEventListener('change', (e) => {
-	newForm.hour2 = e.target.value;
-  console.log(newForm.hour2);
-})
-  
-let hour3 = document.getElementById('hour3')
-hour3.addEventListener('change', (e) => {
-	newForm.hour3 = e.target.value;
-  console.log(newForm.hour3);
-})
-
-let serviceSummary1 = document.getElementById('serviceSummary1')
-serviceSummary1.addEventListener('change', (e) => {
-	newForm.serviceSummary1 = e.target.value;
-  console.log(newForm.serviceSummary1);
-})
-  
-let serviceSummary2 = document.getElementById('serviceSummary2')
-serviceSummary2.addEventListener('change', (e) => {
-	newForm.serviceSummary2 = e.target.value;
-  console.log(newForm.serviceSummary2);
-})
-  
-let serviceSummary3 = document.getElementById('serviceSummary3')
-serviceSummary3.addEventListener('change', (e) => {
-	newForm.serviceSummary3 = e.target.value;
-  console.log(newForm.serviceSummary3);
-})
+submission.employeeName = e.target.value})
 
 let printForm = document.getElementById('printToPDF')
 printForm.style.display = 'none'
 
 document.getElementById('submit').addEventListener("click", async (event) => {
-	console.log('click')
-  submitForm(newForm, formName);
-  
+    console.log('click')
+	  addMore.style.display = 'none'
+    submitForm(submission, formName);
+      
 })
 
 async function submitForm(data, form) {
@@ -138,23 +119,23 @@ async function submitForm(data, form) {
 }
 
 function respond(data) {
-  let formId = data.formId
-  if (formId) {
-    showSuccess()
-    let name = newForm.clientId	  
-    sendNotification(formId, name)
-    clearForm()	  
-  } else {
-    showError(data.error)
+  let id = data.key
+  if (id) {
+    showSuccess(id)
+    sendNotification(id, newForm.clientId, 'individual', 'not urgent')
+    sendNotification(id, newForm.employeeName, 'individual', 'not urgent')
+    sendNotification(id, 'admin', 'staff', 'not urgent')
+    clearForm()	
+		}
   }
-}
 
-function showSuccess() {
+function showSuccess(id) {
+	document.getElementById('returnMessage').style.display = 'block'
   document.getElementById('returnMessage').innerHTML = 'Form has been successfully submitted'
   printForm.style.display = 'inline'
   printForm.addEventListener('click', (e) => {
-    location.href = `https://phoenix-freedom-foundation-backend.webflow.io/completed-forms/consultation-fee-summary?${client}`
-  })
+		console.log(`https://phoenix-freedom-foundation-backend.webflow.io/completed-forms/consultation-fee-summary?id=${id}`)     
+    location.href = `https://phoenix-freedom-foundation-backend.webflow.io/completed-forms/consultation-fee-summary?id=${id}`})
 }
 
 function showError(err) {
@@ -162,13 +143,15 @@ function showError(err) {
     document.getElementById('returnMessage').innerHTML = `An error occurred when submitting this form, which was ${err}. Please contact the administrator for help.`
 }
 
-async function sendNotification(id, client) {
+async function sendNotification(id, recipient, type, priority) {
   let message = `You have a new <br/><a href=phoenix-freedom-foundation-backend.webflow.io/completed-forms/consultation-fee-summary?formId=${id}>Consultation Fee Summary</a>`
   console.log(message)
   const url = 'https://pffm.azurewebsites.net/notices'
   let notification = {
-    'name': client,
-    'notice' : message 
+    'name': recipient,
+    'notice': message,
+    'type': type,
+    'priority': priority
   }
   const header = {
       'Content-Type': 'application/json',
@@ -185,9 +168,6 @@ async function sendNotification(id, client) {
 }
  
 function clearForm() {
-  newForm = {}
-  document.querySelector('input#clientName').value = '';
-  document.querySelector('input#employeeName').value = '';
   for (let i = 1; i < 4; i++) {
     document.getElementById(`date${i}`).innerHTML = ''
     document.getElementById(`min${i}`).innerHTML = ''
